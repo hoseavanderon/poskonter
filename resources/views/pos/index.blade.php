3a17127eb67ad3ef322006e20ceb4e05647fabe8
hoseavanderson <hosea.vanderson2003@gmail.com>
2025-11-01 12:10:55 +0800

    test auto deploy

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -66,7 +66,7 @@
                             ? 'bg-blue-600 text-white dark:bg-blue-700'
                             : 'bg-gray-100 dark:bg-gray-700 text-gray-600 dark:text-gray-300'"
                         class="px-4 py-2 rounded-lg font-semibold transition">
-                        ⚡ Produk Digital
+                        ⚡ Produk Digital Tri
                     </button>
                     <!-- 🧾 Tombol Tutup Buku -->
                     <button @click="handleCloseBook()"
