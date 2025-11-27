aa5b17baebd2fcd371aa3e2f8b7e2ff83b16a3b6
hoseavanderson <hosea.vanderson2003@gmail.com>
2025-11-27 14:58:38 +0800

    asdasd

--- resources/views/riwayat/index.blade.php
+++ resources/views/riwayat/index.blade.php
@@ -275,6 +275,13 @@
                             </div>
                         </template>
 
+                        <template x-if="tfTarikByApp[7]?.tf > 0">
+                            <div class="flex justify-between">
+                                <span class="text-[#9BA8BF]">Seabank TF</span>
+                                <span class="text-[#81ACFF]" x-text="formatCurrency(tfTarikByApp[6].tf)"></span>
+                            </div>
+                        </template>
+
                         <template x-if="tfTarikByApp[9]?.tf > 0">
                             <div class="flex justify-between">
                                 <span class="text-[#9BA8BF]">MyBCA TF</span>
@@ -450,8 +457,9 @@
                                                         <svg xmlns="http://www.w3.org/2000/svg"
                                                             class="h-4 w-4 text-red-300" fill="none"
                                                             viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V5a1
-                                                                                1 0 00-1-1h-4a1 1 0 00-1 1v2m-5 0h16" />
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V5a1
+                                                                                                1 0 00-1-1h-4a1 1 0 00-1 1v2m-5 0h16" />
                                                         </svg>
                                                         Hapus
                                                     </button>
