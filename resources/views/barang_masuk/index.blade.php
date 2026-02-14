877ee27b01b21ce56967c51fba6b84eed3ec9398
hoseavanderson <hosea.vanderson2003@gmail.com>
2026-02-14 11:15:58 +0800

    asdasd

--- resources/views/barang_masuk/index.blade.php
+++ resources/views/barang_masuk/index.blade.php
@@ -361,7 +361,7 @@
                                         <button @click="openHistoryDetail(item.date)"
                                             class="bg-blue-500 hover:bg-blue-400 text-white
                                            text-sm px-4 py-2 rounded-xl transition">
-                                            Lihat Detail
+                                            Detail
                                         </button>
 
                                     </div>
