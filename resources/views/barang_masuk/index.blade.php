17faaf6548817dbf31638aadb6967d8e64dcecdd
hoseavanderson <hosea.vanderson2003@gmail.com>
2026-03-19 12:04:04 +0800

    asdasd

--- resources/views/barang_masuk/index.blade.php
+++ resources/views/barang_masuk/index.blade.php
@@ -280,8 +280,8 @@
                     Data belum disimpan
                 </h3>
                 <p class="text-gray-400 mb-5">
-                    Kamu masih punya data barang masuk.
-                    Kalau keluar sekarang, semua data akan hilang.
+                    Data yang kamu input akan otomatis disimpan sebagai draft.
+                    Kamu bisa kembali lagi tanpa kehilangan data.
                 </p>
 
                 <div class="flex justify-end gap-3">
@@ -290,8 +290,8 @@
                         Batal
                     </button>
 
-                    <button @click="confirmLeave()" class="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-400">
-                        Keluar & Hapus Data
+                    <button @click="confirmLeave()" class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400">
+                        Lanjutkan & Tinggalkan Halaman
                     </button>
                 </div>
             </div>
