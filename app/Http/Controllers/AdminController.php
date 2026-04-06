23a8525d4de86d4b1b0d361ad7ec38b4721a6fe2
hoseavanderson <hosea.vanderson2003@gmail.com>
2026-04-06 20:05:29 +0800

    asdasd

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -100,7 +100,7 @@ class AdminController extends Controller
             ->whereDate('transactions.created_at', $today)
             ->whereIn('transactions.outlet_id', $outletIds)
             ->whereNull('transactions.deleted_at')
-            ->count();
+            ->sum('detail_transaction.qty');
         +DigitalTransaction::whereDate('created_at', $today)
             ->whereIn('outlet_id', $outletIds)
             ->whereNotIn('digital_product_id', $excludedProducts)
